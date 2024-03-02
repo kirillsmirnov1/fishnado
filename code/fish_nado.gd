2a00d5e417a0d0904c43190946023a30207880e6
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-02 10:51:07 +0100

    dynamic fishnado speed

--- code/fish_nado.gd
+++ code/fish_nado.gd
@@ -5,10 +5,19 @@ signal player_entered
 
 
 @export var speed: float = 1
+@export var speed_invisible: float = 10
+@export var speed_change_rate: float = 1
+
+
+@onready var target_speed: float = speed
+@onready var current_speed: float = speed / 3
 
 
 func _process(delta):
-	global_position.x += delta * speed
+	current_speed = move_toward(current_speed, target_speed, speed_change_rate * delta)
+	global_position.x += delta * current_speed
+	print("fishnado current speed " + str(current_speed))
+	pass
 
 
 func set_active(active: bool):
@@ -18,3 +27,15 @@ func set_active(active: bool):
 
 func _on_player_body_entered(body):
 	player_entered.emit()
+
+
+func _on_screen_visibility_screen_entered():
+	print("fishnado visible")
+	target_speed = speed
+	pass # Replace with function body.
+
+
+func _on_screen_visibility_screen_exited():
+	print("fishnado not visible")
+	target_speed = speed_invisible
+	pass # Replace with function body.
