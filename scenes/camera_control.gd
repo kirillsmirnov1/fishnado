38fd1ba84388f7c9d5284f659c6ee5ca280ab66f
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-02 10:54:32 +0100

    camera offset

--- scenes/camera_control.gd
+++ scenes/camera_control.gd
@@ -1,6 +1,7 @@
 class_name CameraControl extends Camera2D
 
+@export var horizontal_offset: int = 50
 @export var target: Node2D
 
 func _process(delta):
-	global_position.x = target.global_position.x
+	global_position.x = target.global_position.x + horizontal_offset
