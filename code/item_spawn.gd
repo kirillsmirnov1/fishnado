62c455139aa4a63974ac7747097b52eb527ca64b
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-02 15:13:56 +0100

    spawning items

--- code/item_spawn.gd
+++ code/item_spawn.gd
@@ -6,6 +6,9 @@ enum ItemType{
 	FishSlowdown
 }
 
+@export var item_scene: PackedScene
+
 func _on_platform_spawn(data: PlatformSpawnData):
-	# todo spawn smth 
-	pass # Replace with function body.
+	var new_item = item_scene.instantiate() as Item
+	add_child(new_item)
+	new_item.global_position = Vector2((data.x_range.x + data.x_range.y) / 2.0, data.y - 15)
