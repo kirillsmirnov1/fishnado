0b2ff27fba20c5bc864636ee84444b9dfc72386d
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-02 11:36:31 +0100

    refactor tile spawn a bit

--- code/level_tiles.gd
+++ code/level_tiles.gd
@@ -5,23 +5,21 @@ class_name LevelTiles extends TileMap
 
 func _ready():
 	var x: int = ground_start_width
-	while(x < 100):
+	while(x < 300):
 		var next_ground_platform_length = randi_range(3, 10)
-		var points: Array
-		
-		for i in range(x, x + next_ground_platform_length):
-			points.append(Vector2i(i, ground_y))
-		
+		spawn_platform(0, ground_y, x, x + next_ground_platform_length)
 		x += next_ground_platform_length
-		set_cells_terrain_connect(0, points, 0, 0)
 		
-		
-		points.clear()
 		var next_ground_gap = randi_range(1, 5)
-		for i in range(x, x + next_ground_gap):
-			points.append(Vector2i(i, 0))
-		
+		spawn_platform(0, 0, x, x + next_ground_gap)
 		x += next_ground_gap
-		set_cells_terrain_connect(0, points, 0, 1)
 		
 	pass
+
+func spawn_platform(terrain: int, y: int, x_from: int, x_to: int):
+	var points: Array
+	for x in range(x_from, x_to):
+		points.append(Vector2i(x, y))
+	set_cells_terrain_connect(0, points, 0, terrain)
+	
+	
