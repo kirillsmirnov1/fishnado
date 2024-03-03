ba5460c557b484c7052a41efd8192b94eb116e01
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-03 15:55:08 +0100

    different platforms

--- code/level_tiles.gd
+++ code/level_tiles.gd
@@ -26,20 +26,31 @@ func update_level():
 		var x_from = x + randi_range(-1, 1)
 		var x_to = x_from + randi_range(3, 5)
 		
-		spawn_platform(1, y, x_from, x_to)
+		spawn_platform(get_terrain_index(y), y, x_from, x_to)
 		
 		x = x_to
 
 
-func spawn_platform(terrain: int, y: int, x_from: int, x_to: int):
+func spawn_platform(terrain: int, height: int, x_from: int, x_to: int):
 	var points: Array
-	for x in range(x_from, x_to):
-		points.append(Vector2i(x, y))
+	var y_from = height
+	var y_to = Globals.lower_y_bound_tiles if terrain == 0 else height
+	
+	for y in range(y_from, y_to + 1):
+		for x in range(x_from, x_to):
+			points.append(Vector2i(x, y))
 	set_cells_terrain_connect(0, points, 0, terrain)
 	
 	var data = PlatformSpawnData.new()
 	data.x_range = Vector2(x_from, x_to) * Globals.tile_pixel_width
-	data.y = y * Globals.tile_pixel_width
+	data.y = height * Globals.tile_pixel_width
 	
 	on_platform_spawn.emit(data)
-	
+
+func get_terrain_index(y: int) -> int:
+	if y <= 2: 	# clouds
+		return 2
+	if y <= 9: 	# platforms
+		return 1
+	else:		# ground
+		return 0
