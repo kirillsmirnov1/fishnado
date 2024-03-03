382040fd6282ba215b5eac67645f812783ddfd9f
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-03 12:12:33 +0100

    tween music volume

--- code/audio_manager.gd
+++ code/audio_manager.gd
@@ -2,6 +2,9 @@
 extends Node
 
 
+@export var music_tween_duration: float = 0.5
+
+
 @export var action_music: AudioStreamPlayer
 @export var idle_music: AudioStreamPlayer
 
@@ -17,13 +20,19 @@ func start_action_music():
 	playing_action_music = true
 	
 	action_music.playing = true
-	idle_music.playing = false
-	pass
+	
+	var tween = create_tween()
+	tween.tween_property(action_music, "volume_db", -15, music_tween_duration)
+	tween.parallel().tween_property(idle_music, "volume_db", -80, music_tween_duration)
+	
 
 func start_idle_music():
 	if not playing_action_music: return
 	playing_action_music = false
 	
-	action_music.playing = false
 	idle_music.playing = true
+	
+	var tween = create_tween()
+	tween.tween_property(action_music, "volume_db", -80, music_tween_duration)
+	tween.parallel().tween_property(idle_music, "volume_db", 0, music_tween_duration)
 	pass
