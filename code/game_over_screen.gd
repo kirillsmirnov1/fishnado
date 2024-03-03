dc0f94854c44fd6ce1b59f785732b583a612d7e0
kirillsmirnov1 <thegaldren@gmail.com>
2024-03-03 16:25:48 +0100

    go fade

--- code/game_over_screen.gd
+++ code/game_over_screen.gd
@@ -4,6 +4,7 @@ class_name GameOverScreen extends CanvasLayer
 signal restart_button_pressed
 
 
+@export var root: Control
 @export var score_label: Label
 
 
@@ -18,3 +19,6 @@ func display(data: GameOverData):
 		score_label.text = "NEW BEST SCORE\nscore: " + str(data.score)
 	else:
 		score_label.text = "score: " + str(data.score) + "\nbest: " + str(data.best_score)
+	
+	var t = create_tween()
+	t.tween_property(root, "modulate", Color.WHITE, 0.5).from(Color.TRANSPARENT)
